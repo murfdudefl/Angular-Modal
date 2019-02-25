bb0deb2aef1279a156cea8ac4b78abe967fa627c
Mark Murphy <markpub@flnet.net>
2019-02-25 07:29:45 -0500

    New files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ModalComponent } from './modal/modal.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ModalComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
-  ],
+    AppRoutingModule,
+    RouterModule.forRoot(
+      [
+        {path: 'home', component: HomeComponent}
+      ]),
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ src/app/modal/modal.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ModalService {
+
+  constructor() { }
+
+  displayModal(title: string, body: string): Observable<boolean> {
+    
+  }
+}
